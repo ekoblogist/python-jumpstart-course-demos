16b819d27272578643542a37f8a8cc3a585c92ab
ekoblogist <ekoblogist@gmail.com>
2019-11-09 20:23:41 +0100

    ignore .vscode

--- apps/01_hello_world/you_try/program.py
+++ apps/01_hello_world/you_try/program.py
@@ -1,6 +1,9 @@
-print(30*'-')
-print(10*' '+'HELLO APP')
-print(30*'-')
+print(30 * "-")
+print(10 * " " + "HELLO APP")
+print(30 * "-")
 print()
-my_name=input("What is your name?  ")
-print('Nice to meet you '+ my_name + '!!!')
+my_name = input("What is your name?  ")
+
+# TODO Try to do it with f strings.
+
+print("Nice to meet you " + my_name + "!!!")
